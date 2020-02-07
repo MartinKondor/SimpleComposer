2350a08f0486abdeb44e14b083f4814517c0f3e2
Martin Kondor <martinkondor@gmail.com>
2020-02-07 11:25:44 +0100

    Adding playable synth

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-(function ($)
+(($) =>
 {
+    const SYNTH = new Tone.Synth().toMaster();
+    let BPM = 130;
 
-    function playNote(note)
+    // Setup
+    Tone.Transport.bpm.value = BPM;
+
+    let playNote = (note) =>
     {
-        
+        SYNTH.triggerAttackRelease(note, '8n');
     }
 
-    $('.piano rect').on('click', function (e)
+    $('.piano rect').on('click', (e) =>
     {
         playNote(e.target.getAttribute('data-note'));
     });
